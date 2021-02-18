ef4bc2bbe9717dc4973e953f5c7d3b167426c8f6
user42 <ander_alvarenga@hotmail.com]>
2021-02-18 16:05:51 +0100

    substr when start is bigger than *s lenght

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/17 15:33:27 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/18 16:04:07 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int		i;
+	unsigned int		slen;
 	char				*substr;
 
 	i = 0;
+	slen = ft_strlen(s);
+	if ((start + len) > slen)
+	{
+		return (ft_calloc(1, sizeof(char)));
+	}
 	substr = ft_calloc(len + 1, sizeof(char));
 	while (*(s + start + i) != '\0' && i < len)
 	{
