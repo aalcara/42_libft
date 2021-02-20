cadec45a05c9f103f30555cc9452ccb10321e80d
user42 <ander_alvarenga@hotmail.com]>
2021-02-20 20:56:10 +0100

    strncmp deal with \0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 16:00:56 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/13 14:38:28 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/20 20:55:34 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		calc = ((*(unsigned char *)(s1 + i)) - (*(unsigned char *)(s2 + i)));
 		if (calc != 0)
-		{
 			return (calc);
-		}
+		else if ((*(unsigned char *)(s1 + i)) == '\0')
+			return (calc);
 		i++;
 	}
 	return (0);
