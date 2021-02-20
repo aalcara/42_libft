0103a028e1bc90b239a2be59b3440348cf7e425c
user42 <ander_alvarenga@hotmail.com]>
2021-02-20 18:26:01 +0100

    putnbr_fd correct function

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,22 +6,48 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 22:20:32 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/17 01:57:52 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/20 17:57:39 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void			ft_putnbr_fd(int n, int fd)
+static unsigned int	divisor(unsigned int num)
 {
-	char		*nstr;
-	int			i;
+	unsigned int	div;
 
-	i = 0;
-	nstr = ft_itoa(n);
-	while (nstr[i] != '\0')
+	div = 1;
+	while (num / div >= 10)
 	{
-		write(fd, &nstr[i], 1);
-		i++;
+		div = div * 10;
+	}
+	return (div);
+}
+
+void				ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	num;
+	unsigned int	div;
+	int				temp;
+
+	if (n == 0)
+		write(fd, "0", 1);
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			num = n * (-1);
+		}
+		else
+			num = n;
+		div = divisor(num);
+		while (div >= 1)
+		{
+			temp = (num / div) + '0';
+			write(fd, &temp, 1);
+			num = num % div;
+			div = div / 10;
+		}
 	}
 }
