e75ec76455cd64b8df7312acb0b11e7204ee3b40
user42 <ander_alvarenga@hotmail.com]>
2021-02-20 16:58:00 +0100

    substr correction

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/18 16:04:07 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/20 16:55:32 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,28 @@
 char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int		i;
-	unsigned int		slen;
+	unsigned int		s_len;
+	unsigned int		real_len;
 	char				*substr;
 
-	i = 0;
-	slen = ft_strlen(s);
-	if ((start + len) > slen)
+	if (s != NULL)
 	{
-		return (ft_calloc(1, sizeof(char)));
+		i = 0;
+		real_len = len;
+		s_len = ft_strlen(s);
+		if (start >= s_len)
+			return (substr = ft_calloc(1, sizeof(char)));
+		if ((s_len - start) < len)
+			real_len = (s_len - start);
+		substr = ft_calloc(real_len + 1, sizeof(char));
+		if (!substr)
+			return (0);
+		while (*(s + start + i) != '\0' && i < len)
+		{
+			*(substr + i) = *(s + i + start);
+			i++;
+		}
+		return (substr);
 	}
-	substr = ft_calloc(len + 1, sizeof(char));
-	while (*(s + start + i) != '\0' && i < len)
-	{
-		*(substr + i) = *(s + i + start);
-		i++;
-	}
-	return (substr);
+	return (NULL);
 }
