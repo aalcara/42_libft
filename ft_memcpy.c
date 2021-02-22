60e853930b15bc57c329e3a4b178bfc87dc8c1fc
user42 <ander_alvarenga@hotmail.com]>
2021-02-22 15:24:40 +0100

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,14 +6,14 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 19:57:48 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/20 15:45:43 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/22 15:24:12 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
+/*{
 	unsigned int	i;
 
 	if (src == NULL || dest == NULL)
@@ -24,5 +24,18 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		*((char *)dest + i) = *((char *)src + i);
 		i++;
 	}
+	return (dest);
+}
+*/
+{
+	char	*csrc;
+	char	*cdest;
+
+	csrc = (char *)src;
+	cdest = (char *)dest;
+	if (!dest && !src)
+		return (NULL);
+	while (n--)
+		*cdest++ = *csrc++;
 	return (dest);
 }
