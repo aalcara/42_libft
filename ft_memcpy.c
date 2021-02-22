131e85a74787cd7cad756e1e97acb2914b68f939
user42 <ander_alvarenga@hotmail.com]>
2021-02-22 15:28:06 +0100

    memcpy norminette

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,27 +6,13 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 19:57:48 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/22 15:24:12 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/22 15:27:44 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
-/*{
-	unsigned int	i;
-
-	if (src == NULL || dest == NULL)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		*((char *)dest + i) = *((char *)src + i);
-		i++;
-	}
-	return (dest);
-}
-*/
 {
 	char	*csrc;
 	char	*cdest;
