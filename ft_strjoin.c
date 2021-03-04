e0394d37fe0856f96aa3f2f1ef776f951a9d8a66
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-04 12:35:24 -0300

    strjoin downgrade

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/03/04 12:29:46 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/03/04 12:34:03 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*char		*ft_strjoin(char const *s1, char const *s2)
+char		*ft_strjoin(char const *s1, char const *s2)
 {
 	unsigned int		i;
 	unsigned int		j;
@@ -37,30 +37,5 @@
 		j++;
 	}
 	*(substr + i + j) = '\0';
-	return (substr);
-}
-*/
-
-char		*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t			s1_len;
-	size_t			s2_len;
-	size_t			total_len;
-	char			*substr;
-
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	if (!(substr = malloc((total_len + 1) * sizeof(char))))
-		return (NULL);
-	while (*s1 != '\0')
-	{
-		*substr++ = *s1++;
-	}
-	while (*s2++ != '\0')
-	{
-		*substr++ = *s2++;
-	}
-	*substr = '\0';
 	return (substr);
 }
