0ea88b0cf2aa876eae0b0a2ab73cded2e446bc59
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-04 10:45:31 -0300

    strjoin_test

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/20 21:05:19 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/03/04 10:44:57 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			*ft_strjoin(char const *s1, char const *s2)
+char		*ft_strjoin(char const *s1, char const *s2)
 {
 	unsigned int		i;
 	unsigned int		j;
@@ -21,11 +21,12 @@ char			*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 && !s2)
+		return (0);
 	len = ft_strlen(s1) + ft_strlen(s2);
-	substr = malloc((len + 1) * sizeof(char));
-	if (substr == NULL)
+	if (!(substr = malloc((len + 1) * sizeof(char))))
 		return (NULL);
-	while (*(s1 + i) != '\0')
+	while (i < ft_strlen(s1))
 	{
 		*(substr + i) = *(s1 + i);
 		i++;
