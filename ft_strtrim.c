c880e8a4e634009e8357bf7ec8f5a526cb370ec1
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-08 17:10:16 -0300

    strtrim test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/03/08 16:52:38 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/03/08 17:09:52 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,15 @@ char				*ft_strtrim(char const *s1, char const *set)
 	size_t			new_len;
 	size_t			new_start;
 
-	if (!s1)
-		return (0);
-	if (!set)
-		return ((char *)s1);
+	if (!s1 || set == NULL)
+		return (NULL);
 	k = ft_strlen(s1) - 1;
 	new_start = 0;
-	while (s1[new_start] != '\0' && ft_strchr(set, s1[new_start]))
+	while (s1[new_start] && ft_strchr(set, s1[new_start]))
 		new_start++;
 	if (new_start == k + 1)
 		return (NULL);
-	while (k >= new_start && ft_strchr(set, s1[k]))
+	while (k && ft_strchr(set, s1[k]))
 		k--;
 	new_len = (k + 1) - new_start;
 	return (ft_substr(s1, new_start, new_len));
@@ -41,8 +39,8 @@ int main (void)
 	char		*set;
 	char		*result;
 
-	s1 = NULL;
-	set = "200";
+	s1 = "kkkkkkkk";
+	set = NULL;
 	result = ft_strtrim(s1, set);
 	printf("|%s|\n", result);
 	return (0);
