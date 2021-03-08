70467d0ed5adc2a0a59c13580cabbb88f7236030
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-08 16:04:28 -0300

    strjoin protection

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:37:47 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/03/08 16:02:32 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/03/08 16:04:15 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,26 +39,3 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	*(substr + i + j) = '\0';
 	return (substr);
 }
-/*
-char		*ft_strjoin(char const *s1, char const *s2)
-{
-	unsigned int		len;
-	char				*substr;
-	char				*substr_zero;
-
-	//if (!s1 && !s2)
-	//	return (0);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(substr = malloc((len + 1) * sizeof(char))))
-		return (NULL);
-	substr_zero = substr;
-	if (s1)
-		while (s1)
-			*substr++ = *s1++;
-	if (s2)
-		while (s2)
-			*substr++ = *s2++;
-	*substr = '\0';
-	return (substr_zero);
-}
-*/
